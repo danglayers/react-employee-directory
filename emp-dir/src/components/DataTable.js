08b90608744af8520b4d8222936953e06f1cc4bf
Donkey <danglayers@gmail.com>
2020-09-22 18:58:20 -0400

    set up base file for DataTable.js

---
+++ emp-dir/src/components/DataTable.js
@@ -0,0 +1,8 @@
+import React, { useContext } from "react";
+import DataBody from "./DataBody";
+import "../styles/DataTable.css";
+import DataAreaContext from "../utils/DataAreaContext";
+
+
+
+export default DataTable;
